f1fbb80d9df7f1e2650444f49f653cc2d638621b
Gabriel Terwesten <gabriel@terwesten.net>
2024-12-10 18:50:36 +0100

    Initial commit

---
+++ example/native_toolchain_meson_example.dart
@@ -0,0 +1,6 @@
+import 'package:native_toolchain_meson/native_toolchain_meson.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ tool/sync_vendored_code.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:logging/logging.dart';
+import 'package:native_toolchain_meson/src/utils/run_process.dart';
+
+const repo = 'https://github.com/blaugold/native';
+const ref = 'meson_builder';
+const destination = 'lib/src/vendor/native_toolchain_c';
+const sourceBasePath = 'pkgs/native_toolchain_c/lib/src';
+final sourcePatterns = [
+  RegExp('tool/.*'),
+  RegExp('native_toolchain/.*'),
+  'cbuilder/compiler_resolver.dart',
+];
+
+void main() async {
+  final logger = Logger('')..onRecord.listen((record) => print(record.message));
+
+  final tempDirectory = Directory.systemTemp.createTempSync();
+
+  try {
+    await runProcess(
+      executable: Uri.file('git'),
+      arguments: [
+        'clone',
+        '--depth',
+        '1',
+        '--branch',
+        ref,
+        repo,
+        tempDirectory.path
+      ],
+      logger: logger,
+    );
+
+    final sourceDirectory = Directory('${tempDirectory.path}/$sourceBasePath');
+    await for (final file in sourceDirectory.list(recursive: true)) {
+      if (file is File) {
+        final relativePath =
+            file.path.substring(sourceDirectory.path.length + 1);
+        if (sourcePatterns
+            .any((pattern) => pattern.matchAsPrefix(relativePath) != null)) {
+          final destinationFile = File('$destination/$relativePath');
+          await destinationFile.parent.create(recursive: true);
+          await file.copy(destinationFile.path);
+        }
+      }
+    }
+  } finally {
+    tempDirectory.deleteSync(recursive: true);
+  }
+}
